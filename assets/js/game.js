562bb9d69045b90ba749c030d0db19a3660605e0
Edb83 <62900492+Edb83@users.noreply.github.com>
2021-08-07 22:56:27 +0100

    Fix player animation to play while moving, but not when landing from a jump

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,12 +154,11 @@ scene("game", ({level, score}) => {
     
     player.action(() => {
         camPos(player.pos)
-        if (player.grounded() && !isMoving) {
-            player.play('idle')
+        if (player.grounded()) {
             isJumping = false
-        }
-        else if (player.grounded() && isMoving) {
-            player.play('run')
+            if (!isMoving) {
+                player.play('idle')
+            } 
         }
         if (player.pos.y > CERTAIN_DEATH/3) {
             camScale(4);
@@ -212,8 +211,11 @@ scene("game", ({level, score}) => {
 
     keyDown('left', () => {
         player.move(-MOVE_SPEED, 0)
-        isMoving = true
         player.scale = vec2(-1, 1)
+        if (!isMoving) {
+            player.play('run')
+            isMoving = true
+        }
     })
 
     keyRelease('left', () => {
@@ -222,8 +224,11 @@ scene("game", ({level, score}) => {
 
     keyDown('right', () => {
         player.move(MOVE_SPEED, 0)
-        isMoving = true
         player.scale = vec2(1, 1)
+        if (!isMoving) {
+            player.play('run')
+            isMoving = true
+        }
     })
 
     keyRelease('right', () => {
