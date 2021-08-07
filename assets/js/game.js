fc61ca08e770bababb5c01d35652a3d00a0b9772
Edb83 <62900492+Edb83@users.noreply.github.com>
2021-08-07 18:58:20 +0100

    Fixed collision detection on baddies - changed to overlap(). Added pipe and semicolon sprites to map

--- assets/js/game.js
+++ assets/js/game.js
@@ -48,7 +48,7 @@ loadSprite("pipeSideBottomEnd", "sprites/pipe-side-bottom-end.png")
 loadSprite("pipeSideBottom", "sprites/pipe-side-bottom.png")
 loadSprite("pipeSideTopEnd", "sprites/pipe-side-top-end.png")
 loadSprite("pipeSideTop", "sprites/pipe-side-top.png")
-loadSprite("pipeUpRight", "sprites/pipe-up-right.png")
+ loadSprite("pipeRight", "sprites/pipe-up-right.png")
 loadSprite("pipeUpTopLeft", "sprites/pipe-up-top-left.png")
 loadSprite("pipeUpTopRight", "sprites/pipe-up-top-right.png")
 loadSprite("semi", "sprites/semi-colon-walking.png", {
@@ -83,12 +83,13 @@ loadSprite("jim", "sprites/super-jim-32x32.png", {
 })
 
 const MOVE_SPEED = 120
-const JUMP_FORCE = 350
+const PLAYER_JUMP_FORCE = 350
 const CERTAIN_DEATH = 1500
 
 const BADDIE_SPEED = 60
+const BADDIE_JUMP_FORCE = 300
  
-let isJumping = false
+let isJumping = true
 let isMoving = false
 
 scene("game", ({level, score}) => {
@@ -96,32 +97,39 @@ scene("game", ({level, score}) => {
     camIgnore(["bg", "ui"]);
 
     const map = [
-        '                                          c                                                        ',
-        '                                                                                                   ',
-        '                                           b                                                             ',
-        '                                                                                                                   ',
-        '                                                     b                                                          ',
-        '                                                                                                              ',
-        '                  c                              b       b                                                     ',
-        '                                              b                                                                    ',
-        '                b                                                                                    c                 ',
-        '    B      b                   y       b                                            c                           c    ',
-        '                                             b                         c                             b     c          ',
-        '                   i          i                      b           c    b      i          c  c                          ',
-        'ggggggggggggggggggggggggg   ggggggggggggggggggggggggg   gggggggggg    gggggggggggggg   g    g    ggggggggggggggggggggg',
+        '                                                                                            ',
+        '                                                                                            ',
+        '                                                                                            ',
+        '                                                                                            ',
+        '                                                                                            ',
+        '                                                                                            ',
+        '                                                                                            ',
+        '                                                                                      12    ',
+        '        12                    12                                                b     lr    ',
+        '        lr          ;         lr                                                      lr    ',
+        'gggggggggggggggggggggggggggggggg   gggggggggg   gggggggggggggg   g  g  ggggggggggggggggggggg',
     ]
 
     const levelCfg = {
-        width: 20,
-        height: 20,
-        'b': [sprite('brick'), solid(), scale(0.75), 'destructible'],
-        'B': [sprite('brownBrick'), solid(), scale(0.75), 'destructible'],
+        width: 32,
+        height: 32,
+        'b': [sprite('brick'), solid(), scale(0.75), 'destructible', 'wall'],
+        'B': [sprite('brownBrick'), solid(), scale(0.75), 'destructible', 'wall'],
         'c': [sprite('coin'), scale(0.75), 'coin'],
-        'g': [sprite('ground'), solid(), scale(1)],
-        'i': [sprite('imposter'), body(), scale(1), 'baddie'],
+        'g': [sprite('ground'), solid()],
+        'i': [sprite('imposter'), body(), {dir: -1}, 'baddie', {timer: 0}],
+        ';': [sprite('semi'), body(), {dir: -1}, 'baddie', {timer: 0}, 'semi'],
         's': [sprite('stack'), scale(1)],
-        'y': [sprite('youtube'), scale(0.75)],
-        // TODO
+        'l': [sprite('pipeLeft'), solid(), scale(1), 'wall'],
+        'r': [sprite('pipeRight'), solid(), scale(1), 'wall'],
+        '1': [sprite('pipeUpTopLeft'), solid(), scale(1), 'next-level'],
+        '2': [sprite('pipeUpTopRight'), solid(), scale(1), 'next-level'],
+        // '1': [sprite('pipeLeftBottom'), solid(), scale(1)],
+        // '2': [sprite('pipeLeftTop'), solid(), scale(1)],
+        // '4': [sprite('pipeSideBottomEnd'), solid(), scale(1)],
+        // '5': [sprite('pipeSideBottom'), solid(), scale(1)],
+        // '6': [sprite('pipeSideTopEnd'), solid(), scale(1)],
+        // '7': [sprite('pipeSideTop'), solid(), scale(1)],
     }
 
     const gameLevel = addLevel(map, levelCfg)
@@ -139,8 +147,6 @@ scene("game", ({level, score}) => {
 
     const player = add([
         sprite('jim'),
-        scale(1),
-        solid(),
         pos(30, 0),
         body(),
         origin('bot'),
@@ -154,7 +160,6 @@ scene("game", ({level, score}) => {
         }
         else if (player.grounded() && isMoving) {
             player.play('run')
-
         }
         if (player.pos.y > CERTAIN_DEATH/3) {
             camScale(4);
@@ -183,16 +188,27 @@ scene("game", ({level, score}) => {
         if (isJumping) {
             play('stomp')    
             destroy(baddie)
-            player.jump(JUMP_FORCE / 2)
-        } else {
-            play('die')
-            go('lose', { score: scoreLabel.value})
+            player.jump(PLAYER_JUMP_FORCE / 2)
         }
     })
 
+    player.overlaps('baddie', () => {
+        play('die')
+        go('lose', { score: scoreLabel.value})
+    })
+
     action('baddie', (baddie) => {
-        baddie.move(-BADDIE_SPEED, 0)
-      })
+        baddie.move(baddie.dir * BADDIE_SPEED, 0)
+        // baddie.timer -= dt()
+        // if (baddie.timer <=0) {
+        //     baddie.jump()
+        //     baddie.timer = rand(10)
+        // }
+    })
+
+    collides('baddie', 'wall', (baddie) => {
+        baddie.dir =- baddie.dir
+    })
 
     keyDown('left', () => {
         player.move(-MOVE_SPEED, 0)
@@ -218,7 +234,7 @@ scene("game", ({level, score}) => {
         if (player.grounded()) {
             play('jump')
             isJumping = true
-            player.jump(JUMP_FORCE)
+            player.jump(PLAYER_JUMP_FORCE)
             player.play('jump')
         }
     })
